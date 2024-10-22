d296c9a1436e644893398a25f6c2ba0985687c98
veer-pratap-fueldao <veer@fueldao.io>
2024-10-22 12:11:29 +0530

    removed email related code

--- src/backend/src/api/reserve_a_car_api.rs
+++ src/backend/src/api/reserve_a_car_api.rs
@@ -1,4 +1,3 @@
-
 use std::collections::BTreeMap;
 
 use candid::Principal;
@@ -6,62 +5,70 @@ use ic_cdk::caller;
 use ic_cdk::{api::time, query};
 use ic_cdk_macros::update;
 
-use crate::{ Car, CarStatus, CustomerDetials, PaymentStatus, RentalTransaction, TransactionHistory, STATE};
-use crate::controller::is_controller;
 use super::monitoring::log_car_checkout;
-use super::send_email::{refresh_token, send_email_gmail};
+use crate::controller::is_controller;
+use crate::{
+    Car, CarStatus, CustomerDetials, PaymentStatus, RentalTransaction, TransactionHistory, STATE,
+};
+// use super::send_email::{refresh_token, send_email_gmail};
 #[update]
-async fn reserve_car(car_id: u64, start_timestamp: u64, end_timestamp: u64,customer :CustomerDetials) -> Result<RentalTransaction, String> {
-    if start_timestamp >= end_timestamp || start_timestamp < (time() / 1_000_000_000 ){
+async fn reserve_car(
+    car_id: u64,
+    start_timestamp: u64,
+    end_timestamp: u64,
+    customer: CustomerDetials,
+) -> Result<RentalTransaction, String> {
+    if start_timestamp >= end_timestamp || start_timestamp < (time() / 1_000_000_000) {
         return Err("Invalid time range".to_string());
     }
 
     customer.validate_details()?;
 
-    let transaction =  STATE.with(|state| {
+    let transaction = STATE.with(|state| {
         let mut state = state.borrow_mut();
 
-        match state.cars.get_mut(&car_id)  {
-            Some(car) => {match  car_availibility( car.clone(), start_timestamp, end_timestamp) {
-                Ok( mut transaction) => {
+        match state.cars.get_mut(&car_id) {
+            Some(car) => match car_availibility(car.clone(), start_timestamp, end_timestamp) {
+                Ok(mut transaction) => {
                     transaction.customer = Some(customer);
-                    car.bookings.insert(transaction.booking_id, transaction.clone());
-                     Ok(transaction)
-                },
-                _ =>  Err("Car is not available".to_string()),
-            }},
-            None => Err("Car not found".to_string())
+                    car.bookings
+                        .insert(transaction.booking_id, transaction.clone());
+                    Ok(transaction)
+                }
+                _ => Err("Car is not available".to_string()),
+            },
+            None => Err("Car not found".to_string()),
         }
     });
+    //
+    // if transaction.is_ok() {
+    //     log_car_checkout(car_id, transaction.as_ref().unwrap().booking_id);
+    //
+    //     let mail_status = send_email_gmail(transaction.clone().unwrap()).await;
+    //
+    //     match mail_status {
+    //         Err(e) if e.contains("invalid_token") => {
+    //             let _ = refresh_token().await;
+    //             let _ = send_email_gmail(transaction.clone().unwrap()).await;
+    //         }
+    //         _ => {}
+    //     }
+    // }
 
-    
-
-    if transaction.is_ok() {
-
-        log_car_checkout(car_id, transaction.as_ref().unwrap().booking_id);
-
-        let mail_status =  send_email_gmail(transaction.clone().unwrap()).await;
-
-        match mail_status {
-            Err(e) if e.contains("invalid_token") => {
-                let _ = refresh_token().await;
-                let _ = send_email_gmail(transaction.clone().unwrap()).await;
-            }
-            _ => {}
-        }
-    }
-
-     transaction
+    transaction
 }
 
-
-pub fn car_availibility(car: Car, start_timestamp: u64, end_timestamp: u64) -> Result<RentalTransaction, String> {
+pub fn car_availibility(
+    car: Car,
+    start_timestamp: u64,
+    end_timestamp: u64,
+) -> Result<RentalTransaction, String> {
     match car.get_booking_status_at_give_time_period(start_timestamp, end_timestamp) {
         CarStatus::Available => {
-            let customer_id = ic_cdk::caller(); 
-           
-            let total_days = (end_timestamp - start_timestamp) as f64 / 86400.0; 
-            let total_amount = car.details.current_price_per_day * total_days as f64;  
+            let customer_id = ic_cdk::caller();
+
+            let total_days = (end_timestamp - start_timestamp) as f64 / 86400.0;
+            let total_amount = car.details.current_price_per_day * total_days as f64;
             let transaction = RentalTransaction {
                 booking_id: time(),
                 car_id: car.id,
@@ -73,30 +80,54 @@ pub fn car_availibility(car: Car, start_timestamp: u64, end_timestamp: u64) -> R
                 payment_status: PaymentStatus::Unpaid,
             };
             return Ok(transaction);
-        },
+        }
         _ => return Err("Car is not available".to_string()),
     }
-
 }
 
-#[query(guard="is_controller")]
+#[query(guard = "is_controller")]
 pub fn all_bookings() -> Vec<BTreeMap<u64, RentalTransaction>> {
     STATE.with(|state| {
-        state.borrow().cars.iter().map(|f| f.1.bookings.clone()).collect()
+        state
+            .borrow()
+            .cars
+            .iter()
+            .map(|f| f.1.bookings.clone())
+            .collect()
     })
 }
 
-#[query(guard="is_controller")]
+#[query(guard = "is_controller")]
 pub fn user_bookings(user: Principal) -> Vec<TransactionHistory> {
     STATE.with(|state| {
-        state.borrow().cars.iter().map(|f| f.1.bookings.clone().iter().filter(|f| f.1.customer_principal_id == user).map(|f| f.1.clone().to_transaction_history()).collect::<Vec<TransactionHistory>>()).flatten().collect()
+        state
+            .borrow()
+            .cars
+            .iter()
+            .map(|f| {
+                f.1.bookings
+                    .clone()
+                    .iter()
+                    .filter(|f| f.1.customer_principal_id == user)
+                    .map(|f| f.1.clone().to_transaction_history())
+                    .collect::<Vec<TransactionHistory>>()
+            })
+            .flatten()
+            .collect()
     })
 }
 
 #[query]
 pub fn booking_details(car_id: u64, booking_id: u64) -> Option<TransactionHistory> {
     STATE.with(|state| {
-        state.borrow().cars.iter().find(|f| *f.0 == car_id).map(|f| f.1.bookings.get(&booking_id)).flatten().map(|f| f.to_transaction_history())
+        state
+            .borrow()
+            .cars
+            .iter()
+            .find(|f| *f.0 == car_id)
+            .map(|f| f.1.bookings.get(&booking_id))
+            .flatten()
+            .map(|f| f.to_transaction_history())
     })
 }
 
@@ -104,6 +135,20 @@ pub fn booking_details(car_id: u64, booking_id: u64) -> Option<TransactionHistor
 pub fn current_user_bookings() -> Vec<RentalTransaction> {
     let user = caller();
     STATE.with(|state| {
-        state.borrow().cars.iter().map(|f| f.1.bookings.clone().iter().filter(|f| f.1.customer_principal_id == user).map(|f| f.1.clone()).collect::<Vec<RentalTransaction>>()).flatten().collect()
+        state
+            .borrow()
+            .cars
+            .iter()
+            .map(|f| {
+                f.1.bookings
+                    .clone()
+                    .iter()
+                    .filter(|f| f.1.customer_principal_id == user)
+                    .map(|f| f.1.clone())
+                    .collect::<Vec<RentalTransaction>>()
+            })
+            .flatten()
+            .collect()
     })
 }
+
--- src/backend/src/lib.rs
+++ src/backend/src/lib.rs
@@ -4,25 +4,24 @@ use api::monitoring::MonitoringState;
 use default::DEFAULT_CAR_ID;
 use ic_cdk::{post_upgrade, pre_upgrade, storage};
 pub use models::*;
-pub mod default;
 mod api;
-mod controller;
 pub mod constant;
+mod controller;
+pub mod default;
 pub use api::monitoring::EventMoniter;
 pub use candid::Principal;
 pub use controller::*;
-use crate::api::send_email::MailState;
-pub mod utils;
+// use crate::api::send_email::MailState;
 #[cfg(test)]
 mod tests;
-
+pub mod utils;
 
 thread_local! {
     static STATE: RefCell<State> = RefCell::new(State {
         cars: BTreeMap::new(),
         monitoring: MonitoringState::default(),
         controllers: Vec::new(),
-        mail_state: None,
+        // mail_state: None,
     });
 }
 
@@ -34,50 +33,58 @@ fn init() {
 fn init_hook() {
     STATE.with(|state| {
         let mut state = state.borrow_mut();
-        
+
         // Parse the hardcoded Principal for the default car
         // let default_car_principal = Principal::from_text(DEFAULT_CAR_PRINCIPAL).expect("Invalid principal format");
-        
+
         // Create the default car
         let default_car = CarDetails::default();
 
-
-        if !state.cars.contains_key(&DEFAULT_CAR_ID )  {
-            state.cars.insert(DEFAULT_CAR_ID, Car { id: DEFAULT_CAR_ID, details: default_car, bookings: BTreeMap::new(),/*  monitoring: Vec::new() */});
+        if !state.cars.contains_key(&DEFAULT_CAR_ID) {
+            state.cars.insert(
+                DEFAULT_CAR_ID,
+                Car {
+                    id: DEFAULT_CAR_ID,
+                    details: default_car,
+                    bookings: BTreeMap::new(), /*  monitoring: Vec::new() */
+                },
+            );
         }
-        
     });
 }
 
 #[pre_upgrade]
 fn pre_upgrade() {
-    STATE.with(|state| storage::stable_save((State {
-        cars: state.borrow().cars.clone(), 
-        monitoring: state.borrow().monitoring.clone(),
-        controllers: state.borrow().controllers.clone(),
-        mail_state: state.borrow().mail_state.clone()
-    },)).unwrap());
+    STATE.with(|state| {
+        storage::stable_save((State {
+            cars: state.borrow().cars.clone(),
+            monitoring: state.borrow().monitoring.clone(),
+            controllers: state.borrow().controllers.clone(),
+            // mail_state: state.borrow().mail_state.clone(),
+        },))
+        .unwrap()
+    });
 }
 
 #[post_upgrade]
 fn post_upgrade() {
-    let state: Result<(State, ), String> = storage::stable_restore();
+    let state: Result<(State,), String> = storage::stable_restore();
     match state {
         Ok(state) => {
-            STATE.with(|s| { *s.borrow_mut() =  state.0;  });
+            STATE.with(|s| {
+                *s.borrow_mut() = state.0;
+            });
             init_hook();
-        }, Err(e) => {
+        }
+        Err(e) => {
             println!("Failed to do post upgrade {e}");
         }
     }
 }
 
-
-
 #[ic_cdk_macros::query]
 fn greet(name: String) -> String {
     format!("Hello, {}!", name)
 }
-
 
 ic_cdk_macros::export_candid!();
