8761dc7236cb0e5b35ae12b154001f16be360b2c
Prakash Bhatt <prk-jr@Prakashs-MacBook-Air.local>
2024-12-06 22:08:56 +0530

    Removed the default api

--- src/backend/src/controller.rs
+++ src/backend/src/controller.rs
@@ -6,7 +6,6 @@ use crate::STATE;
 pub fn is_controller() -> Result<(), String> {
     let caller = ic_cdk::caller();
     if ic_cdk::api::is_controller(&caller){ return  Ok(());}
-    if Principal::from_text("nrfjk-jrfvy-xswr3-unaf5-q4kpm-xadal-76yec-irhz4-s2smp-5cl3a-mae").unwrap() == caller{ return  Ok(());}
     STATE.with(|state| {
         if state
             .borrow()
--- src/backend/src/default.rs
+++ src/backend/src/default.rs
@@ -1,44 +1,44 @@
 
-pub const DEFAULT_CAR_ID: u64 = 0; 
-const DEFAULT_IMAGE_URL : &str = "https://hmh62-uiaaa-aaaai-actxq-cai.icp0.io/public/img/backend/Logo.jpg";
-use ic_cdk_macros::query;
+// pub const DEFAULT_CAR_ID: u64 = 0; 
+// const DEFAULT_IMAGE_URL : &str = "https://hmh62-uiaaa-aaaai-actxq-cai.icp0.io/public/img/backend/Logo.jpg";
+// use ic_cdk_macros::query;
 
-use crate::{Car, CarDetails, CarStatus, CarType, FuelType,  TransmissionType, STATE};
+// use crate::{Car, CarDetails, CarStatus, CarType, FuelType,  TransmissionType, STATE};
 
-impl Default for CarDetails {
-    fn default() -> Self {
-        CarDetails {
-            id: DEFAULT_CAR_ID,
-            make: "MG".to_string(),
-            model: "ZS EV".to_string(),
-            year: 2024,
-            default_image_url: DEFAULT_IMAGE_URL.into(),
-            description: "The 2024 MG ZS EV: where innovation meets style. This all-electric SUV combines cutting-edge technology with a sleek design, making it the perfect choice for the eco-conscious driver who refuses to compromise on performance or comfort. With a robust electric powertrain, the ZS EV delivers a thrilling drive and impressive range, allowing you to explore your world with confidence. Its spacious interior is crafted for convenience, featuring advanced infotainment options and premium materials that elevate every journey.".into(),
-            car_type: CarType::SUV,
-            price_per_day: 6000.0,
-            current_price_per_day: 4000.0,
-            capacity: 5,
-            images: vec![
-                "https://hmh62-uiaaa-aaaai-actxq-cai.icp0.io/public/img/backend/1.jpeg".to_string(),
-                "https://hmh62-uiaaa-aaaai-actxq-cai.icp0.io/public/img/backend/2.jpeg".to_string(),
-                "https://hmh62-uiaaa-aaaai-actxq-cai.icp0.io/public/img/backend/3.jpeg".to_string(),
-                ],
-            status: CarStatus::Available,
-            // last_serviced: Some(Utc::now().timestamp() as u64),
-            mileage: Some(461),
-            fuel_type: FuelType::Electric,
-            transmission_type: TransmissionType::Automatic,
-            color: Some("https://hmh62-uiaaa-aaaai-actxq-cai.icp0.io/public/img/backend/1.jpeg".to_string()),
-            pickup_location: None,
-            dropoff_location: None,
-        }
-    }
-}
+// impl Default for CarDetails {
+//     fn default() -> Self {
+//         CarDetails {
+//             id: DEFAULT_CAR_ID,
+//             make: "MG".to_string(),
+//             model: "ZS EV".to_string(),
+//             year: 2024,
+//             default_image_url: DEFAULT_IMAGE_URL.into(),
+//             description: "The 2024 MG ZS EV: where innovation meets style. This all-electric SUV combines cutting-edge technology with a sleek design, making it the perfect choice for the eco-conscious driver who refuses to compromise on performance or comfort. With a robust electric powertrain, the ZS EV delivers a thrilling drive and impressive range, allowing you to explore your world with confidence. Its spacious interior is crafted for convenience, featuring advanced infotainment options and premium materials that elevate every journey.".into(),
+//             car_type: CarType::SUV,
+//             price_per_day: 6000.0,
+//             current_price_per_day: 4000.0,
+//             capacity: 5,
+//             images: vec![
+//                 "https://hmh62-uiaaa-aaaai-actxq-cai.icp0.io/public/img/backend/1.jpeg".to_string(),
+//                 "https://hmh62-uiaaa-aaaai-actxq-cai.icp0.io/public/img/backend/2.jpeg".to_string(),
+//                 "https://hmh62-uiaaa-aaaai-actxq-cai.icp0.io/public/img/backend/3.jpeg".to_string(),
+//                 ],
+//             status: CarStatus::Available,
+//             // last_serviced: Some(Utc::now().timestamp() as u64),
+//             mileage: Some(461),
+//             fuel_type: FuelType::Electric,
+//             transmission_type: TransmissionType::Automatic,
+//             color: Some("https://hmh62-uiaaa-aaaai-actxq-cai.icp0.io/public/img/backend/1.jpeg".to_string()),
+//             pickup_location: None,
+//             dropoff_location: None,
+//         }
+//     }
+// }
 
-#[query]
-fn get_default_car() -> Option<Car> {
-    STATE.with(|state| {
-        let state = state.borrow();
-        state.cars.get(&DEFAULT_CAR_ID).cloned()
-    })
-}
+// #[query]
+// fn get_default_car() -> Option<Car> {
+//     STATE.with(|state| {
+//         let state = state.borrow();
+//         state.cars.get(&DEFAULT_CAR_ID).cloned()
+//     })
+// }
