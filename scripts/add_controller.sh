52ac67025aeb114d50f8970f9659b96ab0629b60
Prakash Bhatt <prk-jr@Prakashs-MacBook-Air.local>
2024-12-12 14:45:40 +0530

    Updated Workflow to deploy only on merge

--- scripts/add_controller.sh
+++
@@ -1,7 +0,0 @@
-  dfx canister update-settings --add-controller  "nrfjk-jrfvy-xswr3-unaf5-q4kpm-xadal-76yec-irhz4-s2smp-5cl3a-mae" provision --ic
-  dfx canister update-settings --add-controller  "nrfjk-jrfvy-xswr3-unaf5-q4kpm-xadal-76yec-irhz4-s2smp-5cl3a-mae" asset_proxy --ic
-  dfx canister update-settings --add-controller  "nrfjk-jrfvy-xswr3-unaf5-q4kpm-xadal-76yec-irhz4-s2smp-5cl3a-mae" asset --ic
-
-  dfx canister update-settings --add-controller  "o7ydv-5cgvw-a5hp5-wh4wg-7y2lk-tcrwu-5qjag-rse43-g4tam-giqgo-kqe" provision --ic
-  dfx canister update-settings --add-controller  "o7ydv-5cgvw-a5hp5-wh4wg-7y2lk-tcrwu-5qjag-rse43-g4tam-giqgo-kqe" asset_proxy --ic
-  dfx canister update-settings --add-controller  "o7ydv-5cgvw-a5hp5-wh4wg-7y2lk-tcrwu-5qjag-rse43-g4tam-giqgo-kqe" asset --ic
